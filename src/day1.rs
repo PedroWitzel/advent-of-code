2be79074aad1bdf34ddde08f9b37d40846b887d5
WITZEL Pedro <pedro.witzel@alstomgroup.com>
2025-10-21 11:29:21 +0200

    chore: add test

--- src/day1.rs
+++ src/day1.rs
@@ -21,3 +21,13 @@ pub fn part1() -> String {
 
     coordinates.to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), "54877");
+    }
+}
