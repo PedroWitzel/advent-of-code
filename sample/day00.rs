18a22f255cc9a1e4fd7787fa3beb6cd9480499e6
Pedro Witzel <pedro.witzel@gmail.com>
2025-12-01 23:33:10 +0100

    day01 - around the clock we go

--- sample/day00.rs
+++ sample/day00.rs
@@ -32,6 +32,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part1(), "0");
+        assert_eq!(part2(), "0");
     }
 }
--- src/day01.rs
+++ src/day01.rs
@@ -3,13 +3,31 @@ use std::io::{BufRead, BufReader};
 use crate::helpers;
 
 pub fn run() {
-    part1();
-    part2();
+    println!("{}\n", part1());
+    println!("{}\n", part2());
 }
 
 fn part1() -> String {
     let lines = BufReader::new(helpers::input_file(file!())).lines();
     let mut answer = 0;
+
+    lines
+        .map(|s| {
+            let s = s.unwrap();
+            let (side, turn) = s.split_at(1);
+            let mut turn = turn.parse::<i32>().unwrap();
+            if side == "L" {
+                turn = -turn
+            }
+            turn
+        })
+        .fold(50, |acc, e| {
+            let acc = acc + e;
+            if acc % 100 == 0 {
+                answer += 1
+            }
+            acc
+        });
 
     answer.to_string()
 }
@@ -17,6 +35,33 @@ fn part1() -> String {
 fn part2() -> String {
     let lines = BufReader::new(helpers::input_file(file!())).lines();
     let mut answer = 0;
+
+    lines
+        .map(|s| {
+            let s = s.unwrap();
+            let (side, turn) = s.split_at(1);
+            let mut turn = turn.parse::<i32>().unwrap();
+            if side == "L" {
+                turn = -turn
+            }
+            turn
+        })
+        .fold(1000_50, |acc, e| {
+            // Full turns taken into account here
+            answer += (e / 100).abs();
+
+            let next = acc + e % 100;
+
+            // Spot on zero
+            if next % 100 == 0 {
+                answer += 1;
+            } else if acc % 100 != 0 && (acc / 100 - next / 100).abs() != 0 {
+                // Crossed zero
+                answer += 1;
+            }
+
+            next
+        });
 
     answer.to_string()
 }
@@ -27,11 +72,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), "0");
+        assert_eq!(part1(), "992");
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part1(), "0");
+        assert_eq!(part2(), "6133");
     }
 }
